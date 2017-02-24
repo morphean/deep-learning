cb7bee70056051984ac04bcb59ce9afc1a5925ae
Pierre Karadia <info@morphean.co.uk>
2017-02-24 18:03:46 +0000

    part 14 - implementing multilayer perceptron adding simple gpu code for RNG (if cuda is available)

--- neural-network/multilayer_perceptron.py
+++ neural-network/multilayer_perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+from accelerate.cuda import rand, cuda_compatible
 
 def initWeights(noOfInputs, noOfHidden):
     return np.random.normal(0, noOfInputs ** -0.5, size=(noOfInputs, noOfHidden))
@@ -6,11 +7,6 @@ def initWeights(noOfInputs, noOfHidden):
 #input data
 features = []
 
-# number of records and input units
-n_records, n_inputs = features.shape
-
-# number of hidden units
-n_hidden = 2
 
 # network looks like
 #        o              w11 w12
@@ -18,6 +14,42 @@ n_hidden = 2
 #      h1  h2           w31 w32
 #     / \  / \
 #    x1  x2  x3
-#
 
-weights_input_to_hidden = initWeights(n_inputs, n_hidden)
+#Network size
+N_input = 4
+N_hidden = 3
+N_output = 2
+
+np.random.seed(42)
+
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1/(1+np.exp(-x))
+
+shape = (N_input, N_hidden)
+
+# Make some fake data
+if cuda_compatible() is True:
+    X = rand.normal(0, 1, (4))
+else:
+    X = np.random.randn(4)
+
+weights_input_to_hidden = np.random.normal(0, scale=0.1, size=(N_input, N_hidden))
+weights_hidden_to_output = np.random.normal(0, scale=0.1, size=(N_hidden, N_output))
+
+
+# TODO: Make a forward pass through the network
+
+hidden_layer_in = np.dot(X, weights_input_to_hidden)
+hidden_layer_out = sigmoid(hidden_layer_in)
+
+print('Hidden-layer Output:')
+print(hidden_layer_out)
+
+output_layer_in = np.dot(hidden_layer_out, weights_hidden_to_output)
+output_layer_out = sigmoid(output_layer_in)
+
+print('Output-layer Output:')
+print(output_layer_out)
